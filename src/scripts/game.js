b1daea865444abec0b3bbf6aa1ef5ade7fa24c32
joão vitor <shepherdcom12@gmail.com>
2024-02-28 00:03:19 -0300

    add cards animation

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,3 +1,4 @@
+let oneCardSelected = false
 const cardsDiv = document.getElementById("cardsDiv")
 function sortearNumeros() {
     let jokerNumber = Math.floor(Math.random() * 8) + 1;
@@ -9,7 +10,24 @@ function sortearNumeros() {
     return [jokerNumber, aceNumber];
 }
 
+function selectCard(result) {
+    oneCardSelected = true
+    if (result == "lose") {
+        setTimeout(() => {
+            cardsDiv.style.transform = "translateY(100vh)"
+            cardsDiv.style.gap = "0px"
+            setTimeout(() => {                
+                randomCards()
+                oneCardSelected = false
+                cardsDiv.style.transform = "translateY(0vh)"
+                cardsDiv.style.gap = "52px"
+            }, 1000);
+        }, 2000);
+    }
+}
+
 function randomCards() {
+    cardsDiv.innerHTML = ``
     const jokerAceNumber = sortearNumeros();
     for (let index = 0; index < 8; index++) {
         let thisCardNumber = index + 1
@@ -28,27 +46,30 @@ function randomCards() {
         </div>
         `
         card.onclick = function () {
-            card.children[0].style.transform = "rotateY(180deg)"
-            if (thisCardNumber != jokerAceNumber[0] && thisCardNumber != jokerAceNumber[1]) {
-                card.children[0].children[1].innerHTML = `
-                    <span class="frontSide__spanLoseTop">${thisCardNumber}</span>
-                    <span class="frontSide__spanLoseIcon"></span>
-                    <span class="frontSide__spanLoseBotton">${thisCardNumber}</span>`
-            } else {
-                if (thisCardNumber == jokerAceNumber[0]) {
+            if (oneCardSelected == false) {
+                card.children[0].style.transform = "rotateY(180deg)"
+                if (thisCardNumber != jokerAceNumber[0] && thisCardNumber != jokerAceNumber[1]) {
                     card.children[0].children[1].innerHTML = `
-                    <span class="frontSide__spanJokerTop">JOKER</span>
-                    <span class="frontSide__spanJokerIcon"><img src="https://img.hotimg.com/jokerHat.jpeg" alt=""></span>
-                    <span class="frontSide__spanJokerBotton">JOKER</span>`
-                }
-                if (thisCardNumber == jokerAceNumber[1]) {
-                    card.children[0].children[1].innerHTML = `
-                    <span class="frontSide__spanAceTop"><i class="bi bi-suit-spade-fill"></i></span>
-                    <span class="frontSide__spanAceIcon"><i class="bi bi-suit-spade-fill"></i></span>
-                    <span class="frontSide__spanAceBotton"><i class="bi bi-suit-spade-fill"></i></span>`
+                        <span class="frontSide__spanLoseTop">${thisCardNumber}</span>
+                        <span class="frontSide__spanLoseIcon"></span>
+                        <span class="frontSide__spanLoseBotton">${thisCardNumber}</span>`
+                    selectCard('lose')
+                } else {
+                    if (thisCardNumber == jokerAceNumber[0]) {
+                        card.children[0].children[1].innerHTML = `
+                        <span class="frontSide__spanJokerTop">JOKER</span>
+                        <span class="frontSide__spanJokerIcon"><img src="https://img.hotimg.com/jokerHat.jpeg" alt=""></span>
+                        <span class="frontSide__spanJokerBotton">JOKER</span>`
+                    }
+                    if (thisCardNumber == jokerAceNumber[1]) {
+                        card.children[0].children[1].innerHTML = `
+                        <span class="frontSide__spanAceTop"><i class="bi bi-suit-spade-fill"></i></span>
+                        <span class="frontSide__spanAceIcon"><i class="bi bi-suit-spade-fill"></i></span>
+                        <span class="frontSide__spanAceBotton"><i class="bi bi-suit-spade-fill"></i></span>`
+                    }
                 }
             }
         }
-    }    
+    }
 }
 randomCards()
